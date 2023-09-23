9a0e8548a1ddaeecc9ecb62fcad7d32701b6ebd9
Ranjeet Kumar <ranjeet93ks@gmail.com>
2023-09-23 17:40:32 +0530

    m

--- src/main/java/com/exception/handling/api/exception/UserNotFoundException.java
+++ src/main/java/com/exception/handling/api/exception/UserNotFoundException.java
@@ -1,5 +1,6 @@
 package com.exception.handling.api.exception;
 
+@SuppressWarnings("serial")
 public class UserNotFoundException  extends Exception{
     public UserNotFoundException(String message) {
         super(message);
